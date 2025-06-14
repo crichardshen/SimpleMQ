b33ca6e40d4d9dba4e50b67161c092c8c78ba70f
crichardshen <107362738+crichardshen@users.noreply.github.com>
2025-06-14 11:57:53 +0800

    Update ChatMessage.java

--- src/main/java/controller/ChatMessage.java
+++ src/main/java/controller/ChatMessage.java
@@ -1,34 +1,7 @@
 package com.SimpleMQ.Controller;
 
-import com.SimpleMQ.Broker.MQBroker;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@Slf4j
-@RestController
-@RequestMapping("/api/messages")
-public class HttpMessageController {
-    private final MQBroker messageHub;
-
-    @Autowired
-    public HttpMessageController(MQBroker messageHub) {
-        this.messageHub = messageHub;
-    }
-
-    @PostMapping
-    public String receiveMessage(@RequestBody byte[] data,
-                                 @RequestHeader("X-Client-Id") String clientId) {
-//        RequestMessage message = new RequestMessage();
-//        message.setClientID(clientId);
-//        message.setProtocolType(ProtocolType.HTTP);
-//        message.setRequestData(data);
-//        message.setTimestamp(System.currentTimeMillis());
-//        message.setSessionID(UUID.randomUUID().toString());
-//
-//        messageHub.AddToRequestMessageQueue(message);
-//        System.out.println("Received HTTP Message from Client:" + clientId);
-
-        return "Message accepted";
-    }
+// 消息实体类
+public class ChatMessage {
+    private String content;
+    // 要有getter/setter
 }

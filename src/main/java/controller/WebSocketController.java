a6aad6583cf278f4eae9df34b13bada0d64ea7ea
crichardshen <107362738+crichardshen@users.noreply.github.com>
2025-06-14 11:59:44 +0800

    Create WebSocketController.java

---
+++ src/main/java/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.SimpleMQ.Controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+public class WebSocketController {
+//    @MessageMapping("/chat") // 接收消息的地址
+//    @SendTo("/topic/messages") // 广播到的地址
+//    public ChatMessage send(ChatMessage message) {
+//        System.out.println("chatting!");
+//        return message; // 简单原样广播
+//    }
+}
+
+

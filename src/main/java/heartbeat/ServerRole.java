eabe1c23d26905db249b5048f6e84b24b631aea1
crichardshen <107362738+crichardshen@users.noreply.github.com>
2025-06-14 12:13:54 +0800

    Delete src/main/java/heartbeat directory

--- src/main/java/heartbeat/ServerRole.java
+++
@@ -1,6 +0,0 @@
-package heartbeat;
-
-public enum ServerRole {
-    MASTER,     // 主服务器
-    SLAVE       // 备服务器
-} 
--- src/main/java/heartbeat/ServerStatus.java
+++
@@ -1,8 +0,0 @@
-package heartbeat;
-
-public enum ServerStatus {
-    RUNNING,    // 正在运行
-    STARTING,   // 正在启动
-    STOPPING,   // 正在停止
-    STOPPED     // 已停止
-} 
